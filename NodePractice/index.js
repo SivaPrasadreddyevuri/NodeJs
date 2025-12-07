1dd8f74c0240f6312a04b6579477c78a0af3d6a7
SivaPrasad Reddy Devloper <evurisivaprasadreddy3@gmail.com>
2025-12-07 14:08:07 +0530

    practice

--- NodePractice/index.js
+++ NodePractice/index.js
@@ -1,5 +1,16 @@
-require("./Modules/Module1"); //using require to import module and this executes first before executing  code in current file
- const Addition=require("./Modules/Module3"); //using require to import module and this executes first before executing  code in current file
-console.log("helloworld");
-Addition()
-console.log(Addition());
+// require("./Modules/Module1"); //using require to import module and this executes first before executing  code in current file
+//  const Addition=require("./Modules/Module3"); //using require to import module and this executes first before executing  code in current file
+// console.log("helloworld");
+// Addition()
+// console.log(Addition());
+
+const http = require("http");
+const server=http.createServer((req, res) => {
+  res.write("Hello World Welcome!");
+  res.end(" ");
+});
+server.listen("1201",()=>{
+    console.log("Server Cerated"
+
+    )
+})
